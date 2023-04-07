7c3e366c9853b7e8a88761f95521a4b2c7a27a72
summerbug <862677188@qq.com>
2023-04-07 21:28:40 +0800

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const element of linkElements) {
+        if (element.href.slice(0, 1) === '/') {
+            // relative
+            try {
+                const urlObj = new URL(`${baseURL}${element.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            // absolute
+            try {
+                const urlObj = new URL(element.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObject = new URL(urlString)
     const hostPath = `${urlObject.hostname}${urlObject.pathname}`
@@ -7,6 +35,8 @@ function normalizeURL(urlString) {
     return hostPath
 }
 
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
